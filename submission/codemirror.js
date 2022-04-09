47d0ef5ea7a43d2481fcd770ed774b60c5b88070
shoray singhal <shoryasinghall@gmail.com>
2022-04-09 17:53:09 +0530

    done

--- submission/codemirror.js
+++ submission/codemirror.js
@@ -1,28 +1,28 @@
 let values = {
-  placeholder: "Your code goes here.",
+  placeholder: "Your python code goes here.",
+  empty: "",
 };
-
-function updateVal() {
-  console.log(codemirror.getValue());
-}
+let submit = document.getElementById("submit");
 
 function setCode(x) {
   codemirror.setValue(values[x]);
 }
 
-// The codeMirror editor object
 let codemirror = CodeMirror.fromTextArea(
   document.getElementById("code-block"),
   {
     lineNumbers: true,
     lineWrapping: true,
     mode: "python",
-    htmlMode: true,
+    htmlMode: false,
     theme: "isotope",
     tabSize: 4,
     indentUnit: 4,
   }
 );
-
-codemirror.on("change", updateVal);
 setCode("placeholder");
+
+submit.addEventListener("click", function () {
+  console.log(codemirror.getValue());
+  setCode("empty");
+});
